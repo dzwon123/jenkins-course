09a0a9ced87128d5d49d6846f9f668ca67701f1a
Lukasz Dzwoniarski <lukasz.dzwoniarski@liquidcode.co>
2020-03-15 15:13:59 +0100

    init commit

---
+++ nodejs.groovy
@@ -0,0 +1,17 @@
+job('Node js example'){
+    scm('https://github.com/dzwon123/jenkins-app.git'){  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
